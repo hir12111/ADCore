eb870673025d37f87b84cd5b5ada6427d12b20e2
Alan Greer <alan.greer@diamond.ac.uk>
2016-07-27 11:51:56 +0100

    Ensure configure files start out clean.

--- ci/configure.sh
+++ ci/configure.sh
@@ -3,15 +3,20 @@
 # Make sure we exit on any error
 set -e
 
+# Remove the RELEASE.linux-x86_64.Common file
+rm configure/RELEASE.local
+rm configure/RELEASE.linux-x86_64.Common
+rm configure/CONFIG_SITE.linux-x86_64.Common
+
 # Generate the configure/RELEASE.local and configure/CONFIG_SITE.linux-x86_64.Common
 # with the details of where to find various external libraries.
 echo "EPICS_BASE=/usr/lib/epics"             >> configure/RELEASE.local
 echo "HDF5=/usr"                             >> configure/CONFIG_SITE.linux-x86_64.Common
 echo "HDF5_LIB=/usr/lib/x86_64-linux-gnu"    >> configure/CONFIG_SITE.linux-x86_64.Common
 echo "HDF5_INCLUDE=-I/usr/include"           >> configure/CONFIG_SITE.linux-x86_64.Common
-echo "SZIP=/usr"                             >> configure/CONFIG_SITE.linux-x86_64.Common
-echo "SZIP_LIB=/usr/lib/x86_64-linux-gnu"    >> configure/CONFIG_SITE.linux-x86_64.Common
-echo "SZIP_INCLUDE=-I/usr/include"           >> configure/CONFIG_SITE.linux-x86_64.Common
+#echo "SZIP=/usr"                             >> configure/CONFIG_SITE.linux-x86_64.Common
+#echo "SZIP_LIB=/usr/lib/x86_64-linux-gnu"    >> configure/CONFIG_SITE.linux-x86_64.Common
+#echo "SZIP_INCLUDE=-I/usr/include"           >> configure/CONFIG_SITE.linux-x86_64.Common
 echo "XML2_INCLUDE=-I/usr/include/libxml2"   >> configure/CONFIG_SITE.linux-x86_64.Common
 echo "BOOST=/usr"                            >> configure/CONFIG_SITE.linux-x86_64.Common
 echo "BOOST_LIB=/usr/lib/x86_64-linux-gnu"   >> configure/CONFIG_SITE.linux-x86_64.Common
@@ -27,7 +32,4 @@ cat configure/RELEASE.local
 
 echo "======= configure/CONFIG_SITE.linux-x86_64.Common ======================="
 cat configure/CONFIG_SITE.linux-x86_64.Common
-
-# Remove the RELEASE.linux-x86_64.Common file
-rm configure/RELEASE.linux-x86_64.Common
 
