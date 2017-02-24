501bf01199e5e46a6f3d08cf64b9bf03ed6d3ef4
Gary Yendell <gary.yendell@diamond.ac.uk>
2017-02-24 09:56:12 +0000

    Move pvaDriver builder support into new pvaDriver repository

--- etc/builder.py
+++ etc/builder.py
@@ -6,7 +6,7 @@ from iocbuilder.arginfo import *
 from iocbuilder.modules.asyn import Asyn, AsynPort, AsynIP
 from iocbuilder.modules.busy import Busy
 from iocbuilder.modules.calc import Calc
-from iocbuilder.modules.ADBinaries import ADBinaries
+from iocbuilder.modules.ADSupport import ADSupport
 
 __all__ = ['ADCore']
 
@@ -86,15 +86,15 @@ NDDataTypes=["NDInt8", "NDUInt8", "NDInt16", "NDUInt16", "NDInt32",
 
 class ADCore(Device):
     """Library dependencies for ADCore"""
-    Dependencies = (Asyn, Busy, ADBinaries)
+    Dependencies = (Asyn, Busy, ADSupport)
     if Architecture() == "win32-x86" or Architecture() == "windows-x64":
         LibFileList = []
     else:
 #        LibFileList = ['GraphicsMagick', 'GraphicsMagickWand', 'GraphicsMagick++', 'PvAPI', 'sz', 'hdf5', 'NeXus', 'cbfad']
-        LibFileList = ['NeXus']
+        LibFileList = []
         SysLibFileList = ['freetype', 'Xext', 'bz2', 'png12', 'xml2', 'X11', 'gomp', 'z', 'jpeg', 'tiff']
-    LibFileList += ['netCDF', 'ADBase', 'NDPlugin']
-    DbdFileList = ['ADSupport', 'NDPluginSupport']
+    LibFileList += ['ADBase', 'NDPlugin']
+    DbdFileList = ['ADSupport', 'NDPluginSupport', 'NDFileHDF5', 'NDFileJPEG', 'NDFileTIFF', 'NDFileNull']
     AutoInstantiate = True
 
 #############################
@@ -114,42 +114,6 @@ class NDPluginBaseTemplate(AutoSubstitution):
 class NDFileTemplate(AutoSubstitution):
     """Template containing the records of an areaDetector file writing plugin"""
     TemplateFile = 'NDFile.template'
-
-#############################
-
-class pvaDriverTemplate(AutoSubstitution):
-    TemplateFile = "pvaDriver.template"
-
-class pvaDetector(AsynPort):
-    """Creates a pvAccess detector"""
-    Dependencies = (ADCore,pvAccessCPP, pvDatabaseCPP, pvDataCPP, normativeTypesCPP)
-    # This tells xmlbuilder to use PORT instead of name as the row ID
-    UniqueName = "PORT"
-    _SpecificTemplate = pvaDriverTemplate
-    def __init__(self, PORT, PVNAME, BUFFERS = 50, MEMORY = 0, PRIORITY = 0, STACKSIZE = 0, **args):
-        # Init the superclass (AsynPort)
-        self.__super.__init__(PORT)
-        # Update the attributes of self from the commandline args
-        self.__dict__.update(locals())
-        # Make an instance of our template
-        makeTemplateInstance(self._SpecificTemplate, locals(), args)
-
-    # __init__ arguments
-    ArgInfo = ADBaseTemplate.ArgInfo + _SpecificTemplate.ArgInfo + makeArgInfo(__init__,
-        PORT = Simple('Port name for the detector', str),
-        PVNAME = Simple('PV Name', str),
-        BUFFERS = Simple('Maximum number of NDArray buffers to be created for plugin callbacks', int),
-        MEMORY = Simple('Max memory to allocate, should be maxw*maxh*nbuffer for driver and all attached plugins', int),
-        PRIORITY = Simple('Max buffers to allocate', int),
-        STACKSIZE = Simple('Max buffers to allocate', int))
-
-    # Device attributes
-    LibFileList = ['pvaDriver']
-    DbdFileList = ['pvaDriverSupport']
-
-    def Initialise(self):
-        print '# pvaDriverConfig(portName, pvName, maxBuffers, maxMemory, priority, stackSize)'
-        print 'pvaDriverConfig("%(PORT)s", %(PVNAME)s, %(BUFFERS)d, %(MEMORY)d, %(PRIORITY)d, %(STACKSIZE)d)' % self.__dict__
 
 #############################
 
